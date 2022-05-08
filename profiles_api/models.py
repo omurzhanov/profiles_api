b592fb46296c8cbf1afede1b060c6b4fd58e0397
Zalkar <omurzhanovz@gmail.com>
2022-05-08 21:14:48 +0200

    added custom user and manager

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for the user profiles"""
+    
+    def create_user(self, email, name, password=None, **kwargs):
+        """Create a new user"""
+        if not email:
+            raise ValueError('User must have an email address')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password, **kwargs):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password, **kwargs)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return f'{self.last_name} {self.name}'
+    
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
